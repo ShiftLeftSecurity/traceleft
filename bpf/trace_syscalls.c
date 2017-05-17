ba86a034184c567e11526345185ff0ceb03073dd
Suchakra Sharma <suchakra@shiftleft.io>
2017-05-17 11:02:36 -0400

    Add main handlers for syscall events

--- bpf/trace_syscalls.c
+++ bpf/trace_syscalls.c
@@ -29,6 +29,22 @@ struct bpf_map_def SEC("maps/handle_open_progs_ret") handle_open_progs_ret = {
 	.map_flags = 0,
 };
 
+struct bpf_map_def SEC("maps/handle_close_progs") handle_close_progs = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_close_progs_ret") handle_close_progs_ret = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
 struct bpf_map_def SEC("maps/handle_read_progs") handle_read_progs = {
 	.type = BPF_MAP_TYPE_PROG_ARRAY,
 	.key_size = sizeof(__u32),
@@ -54,6 +70,118 @@ struct bpf_map_def SEC("maps/handle_write_progs") handle_write_progs = {
 };
 
 struct bpf_map_def SEC("maps/handle_write_progs_ret") handle_write_progs_ret = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_mkdir_progs") handle_mkdir_progs = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_mkdir_progs_ret") handle_mkdir_progs_ret = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_fchown_progs") handle_fchown_progs = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_fchown_progs_ret") handle_fchown_progs_ret = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_fchownat_progs") handle_fchownat_progs = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_fchownat_progs_ret") handle_fchownat_progs_ret = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_fchmod_progs") handle_fchmod_progs = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_fchmod_progs_ret") handle_fchmod_progs_ret = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_fchmodat_progs") handle_fchmodat_progs = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_fchmodat_progs_ret") handle_fchmodat_progs_ret = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_chmod_progs") handle_chmod_progs = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_chmod_progs_ret") handle_chmod_progs_ret = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_chown_progs") handle_chown_progs = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.key_size = sizeof(__u32),
+	.value_size = sizeof(__u32),
+	.max_entries = 32768,
+	.map_flags = 0,
+};
+
+struct bpf_map_def SEC("maps/handle_chown_progs_ret") handle_chown_progs_ret = {
 	.type = BPF_MAP_TYPE_PROG_ARRAY,
 	.key_size = sizeof(__u32),
 	.value_size = sizeof(__u32),
@@ -129,6 +257,166 @@ int kretprobe__handle_open(struct pt_regs *ctx)
 {
 	u64 pid = bpf_get_current_pid_tgid();
 	bpf_tail_call(ctx, (void *)&handle_open_progs_ret, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kprobe/SyS_close")
+int kprobe__handle_close(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_close_progs, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kretprobe/SyS_close")
+int kretprobe__handle_close(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_close_progs_ret, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kprobe/SyS_mkdir")
+int kprobe__handle_mkdir(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_mkdir_progs, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kretprobe/SyS_mkdir")
+int kretprobe__handle_mkdir(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_mkdir_progs_ret, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kprobe/SyS_chown")
+int kprobe__handle_chown(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_chown_progs, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kretprobe/SyS_chown")
+int kretprobe__handle_chwon(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_chown_progs_ret, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kprobe/SyS_fchown")
+int kprobe__handle_fchown(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_fchown_progs, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kretprobe/SyS_fchown")
+int kretprobe__handle_fchwon(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_fchown_progs_ret, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kprobe/SyS_fchownat")
+int kprobe__handle_fchownat(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_fchownat_progs, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kretprobe/SyS_fchownat")
+int kretprobe__handle_fchownat(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_fchownat_progs_ret, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kprobe/SyS_chmod")
+int kprobe__handle_chmod(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_chmod_progs, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kretprobe/SyS_chmod")
+int kretprobe__handle_chmod(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_chmod_progs_ret, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kprobe/SyS_fchmod")
+int kprobe__handle_fchmod(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_fchmod_progs, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kretprobe/SyS_fchmod")
+int kretprobe__handle_fchmod(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_fchmod_progs_ret, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kprobe/SyS_fchmodat")
+int kprobe__handle_fchmodat(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_fchmodat_progs, pid >> 32);
+	// TODO insert default handler here
+
+	return 0;
+}
+
+SEC("kretprobe/SyS_fchmodat")
+int kretprobe__handle_fchmodat(struct pt_regs *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	bpf_tail_call(ctx, (void *)&handle_fchmodat_progs_ret, pid >> 32);
 	// TODO insert default handler here
 
 	return 0;
