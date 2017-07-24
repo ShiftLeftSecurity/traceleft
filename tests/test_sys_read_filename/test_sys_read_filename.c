ce8a30ce01fa9d89428f569b407b60088f354a9e
Iago López Galeiras <iago@kinvolk.io>
2017-07-24 17:37:13 +0200

    tests: add test for getting filename on read

---
+++ tests/test_sys_read_filename/test_sys_read_filename.c
@@ -0,0 +1,30 @@
+#include "../stampwait.h"
+
+#include <stdio.h>
+
+int main(int argc, const char **argv)
+{
+	int err = stampwait(argv[1]);
+	if (err != 0) {
+		fprintf(stderr, "stampwait failed\n");
+		return 1;
+	}
+	int fd = open("/proc/cpuinfo", O_RDONLY, 0);
+	if (fd < 0) {
+		fprintf(stderr, "open failed\n");
+		return 1;
+	}
+
+	// sleep 10ms so we have time to get the fd_install event
+	usleep(10 * 1000);
+
+	char buf[1];
+
+	if (read(fd, buf, 1) < 0) {
+		close(fd);
+		return -1;
+	}
+
+	close(fd);
+	return 0;
+}
